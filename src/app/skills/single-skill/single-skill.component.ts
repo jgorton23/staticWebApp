c564db9669417ce79458ec87b5a53699e07f29ec
Jacob Gorton <jgorton200@gmail.com>
2022-12-03 22:02:37 -0600

    added code to skill/skills components

--- src/app/skills/single-skill/single-skill.component.ts
+++ src/app/skills/single-skill/single-skill.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-single-skill',
   templateUrl: './single-skill.component.html',
-  styleUrls: ['./single-skill.component.css']
 })
-export class SingleSkillComponent implements OnInit {
+export class SkillComponent implements OnInit {
 
+  @Input()
+  level: number | undefined;
+  
   constructor() { }
+
+  showDetails: boolean = false;
 
   ngOnInit(): void {
   }
 
+  onClick(){
+    this.showDetails = !this.showDetails;
+  }
 }
