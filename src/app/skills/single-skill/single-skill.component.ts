20d5adc6f2d24bf7e91c6f2d3f74aea8e0a50920
Jacob Gorton <jgorton200@gmail.com>
2022-12-03 22:11:59 -0600

    added on hover feedback

--- src/app/skills/single-skill/single-skill.component.ts
+++ src/app/skills/single-skill/single-skill.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-single-skill',
   templateUrl: './single-skill.component.html',
+  styleUrls: ['./single-skill.component.css'],
 })
 export class SingleSkillComponent implements OnInit {
 
