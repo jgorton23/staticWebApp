106f76d543f8c16ac64cb7206967238cd3d96bda
Jacob Gorton <jgorton200@gmail.com>
2022-12-06 16:57:23 -0600

    removed large font on hover and began adding skills descriptions

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -15,12 +15,13 @@ export class SkillsComponent implements OnInit {
   }
 
   skills: skill[] = [
-    {skillName: "Java", years: 4, level: 5, description: ''},
-    {skillName: "Git", years: 5, level: 5, description: ''},
-    {skillName: "Python", years: 1, level: 4, description: ''},
-    {skillName: "SQL", years: 2, level: 3, description: ''},
-    {skillName: "C/C++", years: 2, level: 4, description: ''},
-    {skillName: "Docker", years: 1, level: 2, description: ''},
+    {skillName: "Java", years: 4, level: 5, description: 'I have been learning and using Java since 2017. It was the first language I learned. I have used it in both personal and academic projects.'},
+    {skillName: "Git", years: 5, level: 5, description: 'I have used git for close to 5 years for personal and academic projects, and also in a professional setting during my internship.'},
+    {skillName: "Python", years: 1, level: 4, description: 'I have been learning Python for just over a year. I have used it to develop personal projects and to solve leetcode problems.'},
+    {skillName: "SQL", years: 2, level: 3, description: 'I have been learning and using SQL for over a year. I developed multiple academic projects that relied on SQL, and I also usead it heavily during internship.'},
+    {skillName: "C/C++", years: 2, level: 4, description: 'I have been learning C/C++ for multiple years. I have completed several academic projects using C and C++, including a functional operating system.'},
+    {skillName: "C#", years: 1, level: 4, description: 'I have learned within the past year, but I have picked it up very fast. I have primarily used C# while working at my internship.'},
+    {skillName: "Docker", years: 1, level: 2, description: 'I have just begun to use docker within the past year. I have developed small dockerized projects to familiarize myself with using it.'},
     {skillName: "AWS", years: 1, level: 2, description: ''},
     {skillName: "TypeScript", years: 3, level: 4, description: ''},
     {skillName: "Angular", years: 1, level: 4, description: ''},
