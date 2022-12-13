527ebe628c665d59bcca13e73629a0d052af4077
Jacob Gorton <jgorton200@gmail.com>
2022-12-13 14:54:31 -0600

    added skills component css reference in ts file

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -4,6 +4,7 @@ import { skill } from '../shared/skillInterface';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
 })
 
 export class SkillsComponent implements OnInit {
